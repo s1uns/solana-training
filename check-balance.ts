49f04252b1f1b1a743b48420bcd9cda3414baf92
s1uns <illiateliuk@gmail.com>
2024-05-13 20:09:41 +0300

    Updated the public key retrieving

--- check-balance.ts
+++ check-balance.ts
@@ -5,12 +5,16 @@ import {
   PublicKey,
   clusterApiUrl,
 } from "@solana/web3.js";
+import {
+  getKeypairFromEnvironment,
+  airdropIfRequired,
+} from "@solana-developers/helpers";
 
 const connection = new Connection(clusterApiUrl("devnet"));
 
 console.log(`⚡️ Connected to devnet`);
 
-const publicKey = new PublicKey("5JhJhCj5yXhnZxtPRUvAnzGBfQNJhY2js4HoLVXzBTmG");
+const publicKey = new PublicKey(process.env.PUBLIC_KEY_1!.toString());
 
 const balanceInLamports = await connection.getBalance(publicKey);
 
@@ -19,3 +23,15 @@ const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
 console.log(
   `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
 );
+
+
+// await airdropIfRequired(
+//   connection,
+//   publicKey,
+//   1 * LAMPORTS_PER_SOL,
+//   0.5 * LAMPORTS_PER_SOL
+// );
+
+// console.log(
+//   `💰 The balance for the wallet at address ${publicKey} is ${balanceInSOL} now!`
+// );
