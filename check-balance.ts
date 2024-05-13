876b311b0671c42c39e8f4b455f00f0c51b99ee0
s1uns <illiateliuk@gmail.com>
2024-05-13 20:10:54 +0300

    Some minor formatting fixes

--- check-balance.ts
+++ check-balance.ts
@@ -14,7 +14,7 @@ const connection = new Connection(clusterApiUrl("devnet"));
 
 console.log(`⚡️ Connected to devnet`);
 
-const publicKey = new PublicKey(process.env.PUBLIC_KEY_1!.toString());
+const publicKey = new PublicKey(process.env.PUBLIC_KEY_1 ? process.env.PUBLIC_KEY_1 : "5JhJhCj5yXhnZxtPRUvAnzGBfQNJhY2js4HoLVXzBTmG");
 
 const balanceInLamports = await connection.getBalance(publicKey);
 
