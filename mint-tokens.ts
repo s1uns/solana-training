07fd77367dcaea7ecd9a06d8acb03be70c8ead91
s1uns <illiateliuk@gmail.com>
2024-05-20 22:26:27 +0300

    Mint tokens on a token account

---
+++ mint-tokens.ts
@@ -0,0 +1,36 @@
+import { mintTo } from "@solana/spl-token";
+import "dotenv/config";
+import {
+  getExplorerLink,
+  getKeypairFromEnvironment,
+} from "@solana-developers/helpers";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+const connection = new Connection(clusterApiUrl("devnet"));
+
+// Our token has two decimal places
+const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(10, 2);
+
+const sender = getKeypairFromEnvironment("SECRET_KEY");
+
+// Substitute in your token mint account from create-token-mint.ts
+const tokenMintAccount = new PublicKey(
+  "FbYY1eP8gcDnvMgSRBQs5hV1uEgkX7ssQxDAT339hm8T"
+);
+// Subtitute in a recipient token account you just made
+const recipientAssociatedTokenAccount = new PublicKey(
+    "3t4FCxPeLgRcJBo6uynPajRwedw5PhBbdw7WmYDQhKqi"
+  );
+  
+  const transactionSignature = await mintTo(
+    connection,
+    sender,
+    tokenMintAccount,
+    recipientAssociatedTokenAccount,
+    sender,
+    10 * MINOR_UNITS_PER_MAJOR_UNITS
+  );
+  
+  const link = getExplorerLink("transaction", transactionSignature, "devnet");
+  
+  console.log(`✅ Success! Mint Token Transaction: ${link}`);
+  
